2f4caec0e1a13a85955322a2f45c8bfe571f3ca5
samelliottdlt <network.samdlt@gmail.com>
2017-04-09 01:11:29 -0400

    memoize defaultGridState function resolve #4

--- src/Grid.js
+++ src/Grid.js
@@ -23,23 +23,38 @@ export const ColorGrid = ({colors, cellSize, cellSelected, updateCell, colorSele
   </Group>
 )
 
+let memoizedResult = null;
+
 export const defaultGridState = (cellSize, dimensions) => {
-  const result = [];
-  const rowAmount = dimensions.width / (cellSize);
+  const colors = {
+    primary: 'lightgrey',
+    alternate: 'white'
+  }
+  const result = memoizedResult ? memoizedResult : [];
+  const colAmount = dimensions.width / (cellSize);
   const cellAmount = dimensions.height / (cellSize);
-  for(let i = 0; i < rowAmount; i++) {
-    const row = [];
-    for(let j = 0; j < cellAmount; j++) {
+  for(let i = 0; i < colAmount; i++) {
+    const col = [];
+    for(let j = memoizedResult && memoizedResult[i] ? memoizedResult[i].length : 0 ; j < cellAmount; j++) {
       if(i%2) {
-        if(j%2) row.push('lightgrey')
-        else row.push('white');
+        if(j%2) col.push(colors.primary)
+        else col.push(colors.alternate);
       }
       else {
-        if(j%2) row.push('white')
-        else row.push('lightgrey');
+        if(j%2) col.push(colors.alternate)
+        else col.push(colors.primary);
       }
     }
-    result.push(row);
+    if(!!memoizedResult) {
+      if(i >= result.length) {
+        result.push(col);
+      }
+      else
+        Array.prototype.push.apply(result[i], col);
+    }
+    else
+      result.push(col);
   }
+  memoizedResult = result;
   return result;
 }
